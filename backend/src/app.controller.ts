4b251a91f8e51fa74749ef9972364ba2ccb7154f
Dmitriy Maslenikov <dmitriymaslenikov@gmail.com>
2024-08-04 10:52:22 +0300

    04.08.2024

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  MessagePattern,
+  Payload,
+  Ctx,
+  MqttContext,
+} from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  // @MessagePattern({ cmd: '/energy/response' })
+  // accumulate(data: number[]): number {
+  //   console.log('rrrrrr', data);
+  //   return (data || []).reduce((a, b) => a + b);
+  // }
+  @MessagePattern('/energy/response')
+  getNotifications(@Payload() data: number[], @Ctx() context: MqttContext) {
+    console.log(`Topic: ${context.getTopic()}`);
+  }
 
   @Get()
   getHello(): string {
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,64 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnergysModule } from './energy /energys.module';
+import { Energy } from './energy /energy.entity';
+import { EnergyMetersReadingsModule } from './energyMetersReadings/energyMetersReadings.module';
+import { EnergyMetersReadings } from './energyMetersReadings/energyMetersReadings.entity';
+import { Year } from './years/year.entity';
+import { YearsModule } from './years/years.module';
+import { DataSource } from 'typeorm';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { MqttRequestModule } from './mqttRequest/mqttRequest.module';
+import { InputCircuitBreakerEnergysModule } from './inputCircuitBreakerEnergy/inputCircuitBreakerEnergys.module';
+import { InputCircuitBreakerEnergy } from './inputCircuitBreakerEnergy/inputCircuitBreakerEnergy.entity';
+import { PaidMetersReadings } from './paidMeterReadings/paidMeterReadings.entity';
+import { PaidMetersReadingsModule } from './paidMeterReadings/paidMeterReadings.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    EnergysModule,
+    EnergyMetersReadingsModule,
+    InputCircuitBreakerEnergysModule,
+    YearsModule,
+    MqttRequestModule,
+    PaidMetersReadingsModule,
+    ClientsModule.register([
+      {
+        name: 'MQTT_SERVICE',
+        transport: Transport.MQTT,
+        options: {
+          url: 'mqtt://192.168.1.60:1883',
+          clean: true,
+          connectTimeout: 4000,
+          username: 'dmitriy',
+          password: '626920847',
+          reconnectPeriod: 1000,
+        },
+      },
+    ]),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_USER_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [
+        Energy,
+        EnergyMetersReadings,
+        PaidMetersReadings,
+        InputCircuitBreakerEnergy,
+        Year,
+      ],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
